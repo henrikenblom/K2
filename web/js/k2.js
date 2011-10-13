5e6261ae479a020d6266c4d540ee84df9ca38001
Henrik Enblom <henrik.enblom@alfaprint.se>
2011-10-13 13:50:22 +0200

    Flyttade några javascript-funktioner från default.jsp till k2.js.

--- web/js/k2.js
+++ web/js/k2.js
@@ -280,6 +280,37 @@ function isScrolledIntoView(elem) {
     
 }
 
+function updateTips( t ) {
+    $('.validateTips')
+    .text( t )
+    .addClass( "ui-state-highlight" );
+    setTimeout(function() {
+        $('.validateTips').removeClass( "ui-state-highlight", effectDurationDenominator * 8 );
+    }, effectDurationDenominator * 2 );
+}
+
+function checkLength( o, n, min, max ) {
+    if ( o.val().length > max || o.val().length < min ) {
+        o.addClass( "ui-state-error" );
+        updateTips( n + " måste bestå av minst " +
+            min + " och max " + max + " tecken." );
+        o.focus();
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkRegexp( o, regexp, n ) {
+    if ( !( regexp.test( o.val() ) ) ) {
+        o.addClass( "ui-state-error" );
+        updateTips( n );
+        return false;
+    } else {
+        return true;
+    }
+}
+
 function addAtom(elementname, filename) {
     
     $.ajax({
